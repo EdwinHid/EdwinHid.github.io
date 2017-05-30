562eb70488da17774d84ee6c0f24d759834d16a3
Edwin Hidalgo <edwin_hidalgo@brown.edu>
2017-05-30 14:31:44 -0400

    new edits and photography page created

---
+++ js/script_photography.js
@@ -0,0 +1,27 @@
+$( "#asia, #at_asia" ).click(function() {
+  alert( "Handler for .click() called." );
+  $("#longhorizontal").hide();
+  $("photoDisplay").hide();
+  $("#asiaDisplay").show();
+});
+
+$( "#america, #at_america" ).click(function() {
+  alert( "Handler for .click() called." );
+  $("#longhorizontal").hide();
+  $("photoDisplay").hide();
+  $("#americaDisplay").show();
+});
+
+$( "#europe, #at_europe" ).click(function() {
+  alert( "Handler for .click() called." );
+  $("#longhorizontal").hide();
+  $("photoDisplay").hide();
+  $("#europeDisplay").show();
+});
+
+$( "#blackwhite, #at_blackwhite" ).click(function() {
+  alert( "Handler for .click() called." );
+  $("#longhorizontal").hide();
+  $("photoDisplay").hide();
+  $("#blackwhiteDisplay").show();  
+});
